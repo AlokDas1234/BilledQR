3bc14868c420f141947093dca4c02860d73e5025
Alok Das <contact.alokdas@gmail.com>
2025-09-25 12:10:30 +0530

    added in allowed hosts

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-_ga0!l-^9iaz1p3!%y7narxv1g#!n9+!#osw(qnylf(9izeyb_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [ "billedqr.onrender.com","localhost","127.0.0.1"]
 
 CSRF_TRUSTED_ORIGINS = [
     "https://billedqr.onrender.com",
