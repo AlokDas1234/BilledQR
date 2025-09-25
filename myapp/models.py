4ebe93ed30c5418edd1dba30b12af7da342fae0f
Alok Das <contact.alokdas@gmail.com>
2025-09-25 11:10:27 +0530

    adding validity in making qr

--- myapp/models.py
+++ myapp/models.py
@@ -5,6 +5,7 @@ class Item(models.Model):
     item_code = models.CharField(max_length=100,null=True,blank=True)
     item_name= models.CharField(max_length=100,null=True,blank=True)
     stock_qty= models.IntegerField(null=True,blank=True)
+    valid_qr= models.BooleanField(null=True,blank=True)
 
 class validQrCode(models.Model):
     valid_qr=models.BooleanField(default=True,null=True,blank=True)
--- myapp/views.py
+++ myapp/views.py
@@ -13,7 +13,8 @@ def index(request):
         item_code = request.POST.get("item_code")
         item_name = request.POST.get("item_name")
         stock_qty = request.POST.get("stock_qty")
-        Item.objects.create(item_code=item_code, item_name=item_name, stock_qty=stock_qty)
+
+        Item.objects.create(item_code=item_code, item_name=item_name, stock_qty=stock_qty,valid_qr=True)
         return HttpResponse("Form submitted successfully!")
 
     all_items = Item.objects.all()
@@ -24,7 +25,7 @@ def index(request):
         for i in range(item.stock_qty):
             # print("i:",i)
             # items_with_qr.append({"sl_no",i+1})
-            qr_data = f"Code: {item.item_code}, Name: {item.item_name}, Qty: {item.stock_qty} Sl.No: {i+1}"
+            qr_data = f"Code: {item.item_code}, Name: {item.item_name}, Validity: {item.valid_qr} Sl.No: {i+1}"
             qr_img = qrcode.make(qr_data)
 
             buffer = BytesIO()
@@ -36,6 +37,7 @@ def index(request):
                 "item": item,
                 "sl_no": i + 1,
                 "qr_code": qr_base64
+
             })
 
     return render(request, "myapp/index.html", {"items_with_qr": items_with_qr})
